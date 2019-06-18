b7ca49d593ae02cef2a9dcca054fc63efbd4da89
TreezCode <TreezCode@users.noreply.github.com>
2019-06-18 12:53:16 -0600

    heroku deployment test

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,22 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+const connection;
+
+if (process.env.PORT) {
+
+connection = mysql.createConnection(process.env.PORT);
+
+} else {
+
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+    
+}
 
 // Make connection.
 connection.connect(function(err) {
