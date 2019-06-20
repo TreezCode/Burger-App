881594652c82f3a01819e433a596958854d481db
TreezCode <TreezCode@users.noreply.github.com>
2019-06-19 18:06:52 -0600

    heroku test

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,14 @@ let connection;
 if(process.env.JAWSDB_URL) {
   connection = process.env.JAWSDB_URL
 } else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
 }
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
 
 // Make connection.
 connection.connect(function(err) {
